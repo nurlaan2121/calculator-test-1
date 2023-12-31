cd91f3a9bbaa9178cc3844dc70906e83d89b8c89
Nurislam Toygonbaev <ntoygonbaev098@gmail.com>
2023-12-31 13:28:48 +0600

    last in last

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My names Nurlan");
+        System.out.println("My name is Nurlan");
         CalcService service = new CalcServiceImpl();
         Scanner scanner = new Scanner(System.in);
         Scanner scanForStr = new Scanner(System.in);
