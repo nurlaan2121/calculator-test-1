02f9ca6f43791272ee30d25bfd8e8f74be2477c3
Nurislam Toygonbaev <ntoygonbaev098@gmail.com>
2023-12-31 14:30:33 +0600

    Revert "last in last"
    This reverts commit cd91f3a9bbaa9178cc3844dc70906e83d89b8c89.

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My names Nurlan");
-        System.out.println("My name is Nurlan");
         CalcService service = new CalcServiceImpl();
 
 
